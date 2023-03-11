d0ccf1d0b20ff20433cb8fd51f81d07b432776fd
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-03-11 16:56:18 +0000

    Finish the challenge

--- destructuring.js
+++ destructuring.js
@@ -4,9 +4,70 @@
  */
 
 // Destructuring arrays
+let ages = [30, 26, 27];
+//let john = ages[0];
+//let mary = ages[1];
+//let joe = ages[2];
+let [john, mary, joe] = ages
+console.log(john);
 
 // Destructuring objects
+let jobs = {
+    mike:"designer",
+    jill:"developer",
+    allice:"accountant",
+};
+
+let {mike, jill, allice} = jobs
+console.log(mike, jill, allice);
 
 // Destructuring subsets
+let languages = ["english", "franch", "spanish", "german", "japanese"];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary);
+
+let [, , maryNative, marySecondary] = languages;
+console.log(maryNative, marySecondary);
 
 // Using rest parameter syntax
+let fruits = ["apple", "orange", "banana", "peach", "cherry"];
+let [favorite, secondFavorite,...others] = fruits;
+console.log(favorite);
+console.log(secondFavorite);
+console.log(others);
+
+// Challenge:
+let students = [
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'John',
+        subjects: ['art', 'cad', 'english', 'maths', 'science'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 93, maths: 77, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 87, art: 95},
+    }
+];
+
+const makeList = (arr, student) => {
+  const { subjects } = arr.find(({ name }) => name === student);
+  return subjects;
+};
+
+let [first, second, ...rest] = makeList(students, 'John');
+console.log(first, second, rest);
